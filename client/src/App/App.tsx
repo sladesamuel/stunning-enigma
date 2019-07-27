131094395c88b33b338ba8cb86dcdad04df563cc
Samuel Slade <samuel.pjm.slade@googlemail.com>
2019-07-27 22:48:41 +0100

    Setup basic routing and Home, Login and NotFound pages

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,8 +1,18 @@
 import React from "react"
-import { Login } from "../pages"
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom"
+import { Home, Login, NotFound } from "../pages"
+import { isAuthenticated } from "../authentication";
 
 const App = () => (
-  <Login />
+  <BrowserRouter>
+    {!isAuthenticated() && <Redirect to="/login" />}
+
+    <Switch>
+      <Route path="/" exact component={Home} />,
+      <Route path="/login" exact component={Login} />,
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App
