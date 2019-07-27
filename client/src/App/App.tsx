c80aae9d195c29a76791e59c6fc969293af28629
Samuel Slade <samuel.pjm.slade@googlemail.com>
2019-07-27 23:27:38 +0100

    Added app theme

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,18 +1,22 @@
 import React from "react"
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom"
+import { ThemeProvider } from "@material-ui/styles"
+import AppTheme from "../theme"
 import { Home, Login, NotFound } from "../pages"
-import { isAuthenticated } from "../authentication";
+import { isAuthenticated } from "../authentication"
 
 const App = () => (
-  <BrowserRouter>
-    {!isAuthenticated() && <Redirect to="/login" />}
+  <ThemeProvider theme={AppTheme}>
+    <BrowserRouter>
+      {!isAuthenticated() && <Redirect to="/login" />}
 
-    <Switch>
-      <Route path="/" exact component={Home} />,
-      <Route path="/login" exact component={Login} />,
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />,
+        <Route path="/login" exact component={Login} />,
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  </ThemeProvider>
 )
 
 export default App
--- client/src/pages/Login/Login.stories.tsx
+++ client/src/pages/Login/Login.stories.tsx
@@ -1,8 +1,11 @@
 import React from "react"
 import { storiesOf } from "@storybook/react"
+import { muiTheme } from "storybook-addon-material-ui"
+import AppTheme from "../../theme"
 import Login from "./Login"
 
 storiesOf("pages/Login", module)
+  .addDecorator(muiTheme(AppTheme))
   .add("default", () => (
     <Login />
   ))
