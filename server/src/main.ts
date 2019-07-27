a65f99c809b5a9f35d5fc7e0d605772ed05f44a3
Samuel Slade <samuel.pjm.slade@googlemail.com>
2019-07-28 00:28:04 +0100

    Initial commit of the node server

---
+++ server/src/main.ts
@@ -0,0 +1,4 @@
+// Currently just about to start the Webpack section
+// https://medium.com/free-code-camp/build-an-apollo-graphql-server-with-typescript-and-webpack-hot-module-replacement-hmr-3c339d05184f
+
+console.log("Hello, World")
