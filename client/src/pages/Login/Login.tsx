8945bbcf202b3312b3884b596bbf5afa0c937341
Samuel Slade <samuel.pjm.slade@googlemail.com>
2019-07-27 23:07:07 +0100

    Login page uses swipeable views and material UI

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,7 +1,31 @@
 import React, { memo } from "react"
+import SwipeableViews from "react-swipeable-views"
+import { Paper, Tabs, Tab } from "@material-ui/core"
+import LockOpenIcon from "@material-ui/icons/LockOpen"
+import VpnKeyIcon from "@material-ui/icons/VpnKey"
 
-const Login = () => (
-  <div>Login</div>
-)
+const Login = () => {
+  const [tabIndex, setTabIndex] = React.useState(0)
+
+  return (
+    <Paper square>
+      <Tabs
+        value={tabIndex}
+        onChange={(_, newIndex) => setTabIndex(newIndex)}
+        variant="fullWidth"
+        indicatorColor="primary"
+        textColor="primary"
+      >
+        <Tab label="ENTER ACCESS KEY" icon={<VpnKeyIcon />} />
+        <Tab label="LOGIN" icon={<LockOpenIcon />} />
+      </Tabs>
+
+      <SwipeableViews index={tabIndex} onChangeIndex={newIndex => setTabIndex(newIndex)}>
+        <div>Enter Access Key</div>
+        <div>Login</div>
+      </SwipeableViews>
+    </Paper>
+  )
+}
 
 export default memo(Login)
