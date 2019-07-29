56370c2e944a8571ef49c3b322233f81f17740ef
Samuel Slade <samuel.pjm.slade@googlemail.com>
2019-07-29 17:36:14 +0100

    Setup webpack and hot module replacement for the server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,9 @@
 // Currently just about to start the Webpack section
 // https://medium.com/free-code-camp/build-an-apollo-graphql-server-with-typescript-and-webpack-hot-module-replacement-hmr-3c339d05184f
 
-console.log("Hello, World")
+console.log("Hello, World!")
+
+if (module.hot) {
+  module.hot.accept()
+  module.hot.dispose(() => console.log("Module disposed"))
+}
