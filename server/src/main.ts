1d58f10a874ad92c13b0055f8a074bf71203e3bd
Samuel Slade <samuel.pjm.slade@googlemail.com>
2019-07-29 17:49:59 +0100

    Setup environment variables

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,12 +2,18 @@
 
 import { ApolloServer } from "apollo-server"
 
+import { environment } from "./environment"
 import resolvers from "./resolvers"
 import schemas from "./schemas"
 
-const server = new ApolloServer({ resolvers, typeDefs: schemas })
+const server = new ApolloServer({
+  resolvers,
+  typeDefs: schemas,
+  introspection: environment.apollo.introspection,
+  playground: environment.apollo.playground
+})
 
-server.listen()
+server.listen(environment.port)
   .then(({ url }) => console.log(`Server is listening on ${url}`))
 
 if (module.hot) {
