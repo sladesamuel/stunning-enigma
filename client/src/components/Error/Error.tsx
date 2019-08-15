52d546fd07e0beda6e8d05b27448544bcd0f07c1
Samuel Slade <samuel.pjm.slade@googlemail.com>
2019-08-15 23:59:17 +0100

    Styled the error component

--- client/src/components/Error/Error.tsx
+++ client/src/components/Error/Error.tsx
@@ -1,11 +1,37 @@
 import React, { memo } from "react"
+import {
+  Theme, createStyles, WithStyles,
+  withStyles, Toolbar
+} from "@material-ui/core"
+import ErrorIcon from "@material-ui/icons/Error"
 
-interface ErrorProps {
+const styles = (theme: Theme) => createStyles({
+  root: {
+    backgroundColor: theme.palette.error.main,
+    marginBottom: theme.spacing(2),
+    borderRadius: 3
+  },
+  icon: {
+    color: theme.palette.common.white
+  },
+  text: {
+    color: theme.palette.common.white,
+    marginLeft: theme.spacing(2)
+  }
+})
+
+interface ErrorProps extends WithStyles<typeof styles> {
   error: string
 }
 
-const Error = ({ error }: ErrorProps) => (
-  <div>{error}</div>
+const Error = ({ classes, error }: ErrorProps) => (
+  <Toolbar className={classes.root}>
+    <ErrorIcon className={classes.icon} />
+
+    <div className={classes.text}>
+      {error}
+    </div>
+  </Toolbar>
 )
 
-export default memo(Error)
+export default memo(withStyles(styles)(Error))
