cfcdbfa72168793390dc6e7158580f9ca9d8a0c9
Samuel Slade <samuel.pjm.slade@googlemail.com>
2019-08-20 17:50:40 +0100

    Send user invite form

--- client/src/pages/InviteUsers/InviteUsersPage.tsx
+++ client/src/pages/InviteUsers/InviteUsersPage.tsx
@@ -1,7 +1,44 @@
 import React, { memo } from "react"
+import { TextField, Typography, Button } from "@material-ui/core"
 
-const InviteUsersPage = () => (
-  <div>TODO: Invite Users</div>
-)
+interface InviteModel {
+  email?: string
+}
+
+const InviteUsersPage = () => {
+  const [model, setModel] = React.useState<InviteModel>({})
+
+  const handleFieldChange = name => event =>
+    setModel({ ...model, [name]: event.target.value })
+
+  return (
+    <form>
+      <Typography variant="h1">
+        Invite a User
+      </Typography>
+
+      <Typography variant="body1">
+        Enter the details below to invite a user to make a prediction on the birth of the child
+      </Typography>
+
+      <TextField
+        label="Email"
+        value={model.email}
+        onChange={handleFieldChange("email")}
+        autoFocus
+        fullWidth
+      />
+
+      <div style={{ textAlign: "right" }}>
+        <Button
+          variant="outlined"
+          color="primary"
+        >
+          Send Invite
+        </Button>
+      </div>
+    </form>
+  )
+}
 
 export default memo(InviteUsersPage)
