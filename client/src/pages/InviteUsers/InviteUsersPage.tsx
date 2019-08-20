e78415f5c81e3595e0fd14ec0d8fa2e907336e2b
Samuel Slade <samuel.pjm.slade@googlemail.com>
2019-08-20 18:11:29 +0100

    Added mutation into invite page

--- client/src/pages/InviteUsers/InviteUsersPage.tsx
+++ client/src/pages/InviteUsers/InviteUsersPage.tsx
@@ -1,43 +1,87 @@
 import React, { memo } from "react"
+import { Redirect } from "react-router-dom"
+import { useMutation } from "@apollo/react-hooks"
 import { TextField, Typography, Button } from "@material-ui/core"
+import gql from "graphql-tag"
+import { Loading, Error } from "../../components";
+
+const CREATE_USER_INVITE = gql`
+  mutation createUserInvite($email: String!) {
+    createUserInvite(email: $email) {
+      reference
+    }
+  }
+`
 
 interface InviteModel {
   email?: string
 }
 
 const InviteUsersPage = () => {
+  const [complete, setComplete] = React.useState(false)
   const [model, setModel] = React.useState<InviteModel>({})
+
+  const [createUserInvite, result] = useMutation(
+    CREATE_USER_INVITE,
+    { onCompleted: () => setComplete(true) }
+  )
 
   const handleFieldChange = name => event =>
     setModel({ ...model, [name]: event.target.value })
 
+  const canSave = (): boolean => !!model.email
+
+  const save = (e: any) => {
+    e.preventDefault()
+
+    if (!canSave()) {
+      return
+    }
+
+    createUserInvite({
+      variables: {
+        ...model
+      }
+    })
+  }
+
   return (
-    <form>
-      <Typography variant="h1">
-        Invite a User
-      </Typography>
+    <>
+      {complete && <Redirect to="/" />}
+      {result.loading && <Loading />}
+      {!!result.error && <Error error={result.error.message} />}
 
-      <Typography variant="body1">
-        Enter the details below to invite a user to make a prediction on the birth of the child
-      </Typography>
+      <form onSubmit={e => save(e)}>
+        <Typography variant="h1">
+          Invite a User
+        </Typography>
 
-      <TextField
-        label="Email"
-        value={model.email}
-        onChange={handleFieldChange("email")}
-        autoFocus
-        fullWidth
-      />
+        <Typography variant="body1">
+          Enter the details below to invite a user to make a prediction on the birth of the child
+        </Typography>
 
-      <div style={{ textAlign: "right" }}>
-        <Button
-          variant="outlined"
-          color="primary"
-        >
-          Send Invite
-        </Button>
-      </div>
-    </form>
+        <TextField
+          type="Email"
+          label="Email"
+          value={model.email}
+          onChange={handleFieldChange("email")}
+          disabled={result.loading}
+          autoFocus
+          fullWidth
+        />
+
+        <div style={{ textAlign: "right" }}>
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={e => save(e)}
+            disabled={result.loading || !canSave()}
+          >
+            Send Invite
+          </Button>
+        </div>
+      </form>
+    </>
   )
 }
 
